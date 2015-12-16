a9a44fbedb20374b7d56e9a11f066931a98c0c82
monstermonk <kanuj507@gmail.com>
2015-12-16 09:51:44 +0530

    V_16/12/15_home
    LinkList - insertation

--- src/linklist/LinkListTest.java
+++ src/linklist/LinkListTest.java
@@ -1,5 +1,7 @@
 package linklist;
 
+import java.util.concurrent.LinkedTransferQueue;
+
 class Node
 {
 	static Node root=null;
@@ -10,7 +12,7 @@ class Node
 public class LinkListTest {
 
 	
-	private void createNode(int value)
+	private static Node createNode(int value)
 	{
 		Node node =new Node();
 		if(Node.root==null)
@@ -21,21 +23,41 @@ public class LinkListTest {
 		node.prev=null;
 		node.next=null;
 		System.out.println("New node with value"+value+" is created");
+		return node;
+		
 	}
 	
 	public void insertNodeAtEnd(int value)
 	{
-		
+		Node node=LinkListTest.createNode(value);
+		Node iterator=Node.root;
+		while(iterator.next!=null)
+		{
+			iterator=iterator.next;
+		}
+		iterator.next=node;
+		node.prev=iterator;
 	}
 	
 	public void insertNodePosition(int value,int Position)
 	{
-		
+		Node node=LinkListTest.createNode(value);
+		Node iterator=Node.root;
+		while(Position>0)
+		{
+			iterator=iterator.next;
+		}
+		node.next=iterator.next;
+		node.prev=iterator;
+		iterator.next=node;
 	}
 	
 	public void insertNodeAtStart(int value)
 	{
-		
+		Node node=LinkListTest.createNode(value);
+		node.next=Node.root;
+		Node.root.prev=node;
+		Node.root=node;
 	}
 	
 	
@@ -46,6 +68,8 @@ public class LinkListTest {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		insertNo
 		
 	}
 }
+
